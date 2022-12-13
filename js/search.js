8ab178f4f1d7e1f6ec846440dabad29972d04762
James McClung <james@themcclungs.net>
2022-12-13 14:56:57 -0500

    v1.9.0: ranger overhaul again

--- js/search.js
+++ js/search.js
@@ -1,20 +1,50 @@
 function toggleSearch(table, col, event) {
     consumeEvent(event);
     let th = table.rows[0].getElementsByTagName("th")[col];
-    th.getElementsByTagName("span")[0].classList.toggle("displaynone")
-    th.getElementsByTagName("input")[0].classList.toggle("displaynone")
+    th.getElementsByClassName("col-title")[0].classList.toggle("displaynone")
+    let inputField = th.getElementsByClassName("searchbar")[0];
+    if (!inputField.classList.toggle("displaynone")) {
+        inputField.focus();
+    } else {
+        th.getElementsByClassName("searchbutton")[0].focus();
+    }
 }
 
 function consumeEvent(event) {
     event.stopPropagation();
 }
 
-function updateFilter(table, col, input) {
-    let searchval = input.value;
-    for (let row of Array.prototype.slice.call(table.rows, 1)) {
-        let text = row.children[col].innerText.toLowerCase();
+function popupSearch(el, searchval) {
+    anchors = el.getElementsByClassName("popupAnchor");
+    if (anchors.length > 0) {
+        let popup = document.getElementById(anchors[0].onclick.toString().match(/popup\d+/)[0])
+        return popup.innerText.toLowerCase().includes(searchval);
+    }
+    return false;
+}
 
-        if (text.includes(searchval) || searchval == "0" && text.includes("cantrip")) {
+function updateFilter(table, col, input, event) {
+    if (event.key === "Escape") {
+        toggleSearch(table, col, event);
+        return;
+    }
+    let searchval = input.value.toLowerCase();
+    let doPopupSearch = searchval.startsWith("$");
+    if (doPopupSearch) {
+        searchval = searchval.slice(1);
+    }
+
+    for (let row of Array.prototype.slice.call(table.rows, 1)) {
+        let matchedRow;
+
+        if (doPopupSearch) {
+            matchedRow = popupSearch(row.children[col], searchval);
+        } else {
+            let text = row.children[col].innerText.toLowerCase();
+            matchedRow = text.includes(searchval) || searchval == "0" && text.includes("cantrip");
+        }
+
+        if (matchedRow) {
             row.classList.remove(`displaynone${col}`);
         } else {
             row.classList.add(`displaynone${col}`);
